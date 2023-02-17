76302911e0478a79ffcbe8161b647c52204a0198
harsh <harshagarwal26052001@gmail.com>
2023-02-18 03:21:33 +0530

    changed

---
+++ Solutions.cpp
@@ -0,0 +1,2 @@
+1.-> Algorithm DNS(sort 0's,1's and 2's)
+here we have to maintain three pointer and store the 0 values in the front by swaping of elements zero is swapped to be moved to the starting indexes and 2 is swapped to move to the end indexes
