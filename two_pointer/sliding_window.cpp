dc3c1e391df52868ab7ead77738cdf48b9d867ec
harsh <harshagarwal26052001@gmail.com>
2023-03-23 02:19:47 +0530

    added questions on 22-03-2023

--- two_pointer/sliding_window.cpp
+++ two_pointer/sliding_window.cpp
@@ -126,3 +126,37 @@ int numberOfSubstrings(string s)
     }
     return count;
 }
+
+// Very important question
+// Sliding window maximum
+// Given a k in every iteration we have to find the maximum element present in the window of size k
+// Approach Here we use the data structure deque which will store the maximum element in the front() and all the elements which are smaller than the first element in the back in a sorted order of non-decreasing order
+vector<int> maxSlidingWindow(vector<int> &nums, int k)
+{
+    vector<int> ans;
+    deque<int> dq;
+    int i = 0, j = 0;
+    while (j < nums.size())
+    {
+        while (!dq.empty() && nums[j] > dq.back())
+        {
+            dq.pop_back();
+        }
+        dq.push_back(nums[j]);
+        if (j - i + 1 < k)
+        {
+            j++;
+        }
+        else
+        {
+            ans.push_back(dq.front());
+            if (dq.front() == nums[i])
+            {
+                dq.pop_front();
+            }
+            i++;
+            j++;
+        }
+    }
+    return ans;
+}
