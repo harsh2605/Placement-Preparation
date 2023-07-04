b988452985643b4a59adcea106178e60da4d4aac
harsh <harshagarwal26052001@gmail.com>
2023-07-04 11:32:05 +0530

    added trees question

---
+++ tree.cpp
@@ -0,0 +1,328 @@
+// Creation of tree
+#include <bits/stdc++.h>
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(3);
+    root->right = new Node(4);
+}
+
+// Level order traversal of a tree
+void level(struct Node *root)
+{
+    vector<int> ans;
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int size = q.size();
+        for (int i = 0; i < size; i++)
+        {
+            Node *ele = q.front();
+            q.pop();
+            ans.push_back(ele->data);
+            if (ele->left != NULL)
+            {
+                q.push(ele->left);
+            }
+            if (ele->right != NULL)
+            {
+                q.push(ele->right);
+            }
+        }
+    }
+}
+
+// Iterative preorder traversal
+void preorder(struct Node *root)
+{
+    stack<Node *> st;
+    st.push(root);
+    while (!st.empty())
+    {
+        Node *ele = st.top();
+        st.pop();
+        cout << ele->data << " ";
+        if (ele->right != NULL)
+        {
+            st.push(ele->right);
+        }
+        if (ele->left != NULL)
+        {
+            st.push(ele->left);
+        }
+    }
+}
+
+// Iterative inorder traversal
+vector<int> inorderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    stack<TreeNode *> st;
+    TreeNode *node = root;
+    while (true)
+    {
+        if (node != NULL)
+        {
+            st.push(node);
+            node = node->left;
+        }
+        else
+        {
+            if (st.empty())
+                break;
+            node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            node = node->right;
+        }
+    }
+    return ans;
+}
+
+// Postorder traversal using 2 stack
+vector<int> postorderTraversal(TreeNode *root)
+{
+    if (root == NULL)
+        return {};
+    vector<int> ans;
+    stack<TreeNode *> st1, st2;
+    st1.push(root);
+    while (!st1.empty())
+    {
+        TreeNode *node = st1.top();
+        st1.pop();
+        st2.push(node);
+        if (node->left != NULL)
+        {
+            st1.push(node->left);
+        }
+        if (node->right != NULL)
+        {
+            st1.push(node->right);
+        }
+    }
+    while (!st2.empty())
+    {
+        ans.push_back(st2.top()->val);
+        st2.pop();
+    }
+    return ans;
+}
+
+// Inorder preorder and postorder data in one traversal using a single stack
+void solve(TreeNode *root)
+{
+    stack<pair<TreeNode *, int>> st;
+    st.push({root, 1});
+    vector<int> pre, in, post;
+    while (!st.empty())
+    {
+        TreeNode *dir = st.top();
+        st.pop();
+        if (dir.second == 1)
+        {
+            pre.push_back(dir.first->val);
+            dir.second++;
+            st.push(dir);
+            if (dir.first->left != NULL)
+            {
+                st.push(dir.first->left);
+            }
+        }
+        else if (dir.second == 2)
+        {
+            in.push_back(dir.first->val);
+            dir.second++;
+            st.push(dir);
+            if (dir.first->right != NULL)
+            {
+                st.push(dir.first->right);
+            }
+        }
+        else
+        {
+            post.push_back(dir.first->val);
+        }
+    }
+}
+
+// maximum depth of a binary tree
+int solve(TreeNode *root)
+{
+    if (root == NULL)
+        return 0;
+    int lh = solve(root->left);
+    int rh = solve(root->right);
+    return 1 + max(lh, rh);
+}
+
+// Check if the tree is balanced or not
+// conditon of a tree to be balanced (abs(height(left subtree)-height(right subtree))<=1)
+int solve(TreeNode *root)
+{
+    if (root == NULL)
+        return 0;
+    int lh = solve(root->left);
+    int rh = solve(root->right);
+    if (lh == -1 || rh == -1)
+        return -1;
+    if (abs(lh - rh) > 1)
+        return -1;
+    return 1 + max(lh, rh);
+}
+bool balanced(TreeNode *root)
+{
+    if (solve(root) == -1)
+        return false;
+    return true;
+}
+
+// diameter of a bianry tree(it represent the maximum length of the distance of the node which is present far from each other)
+int height(TreeNode *root, int &maxi)
+{
+    if (root == NULL)
+        return 0;
+    int lh = height(root->left, maxi);
+    int rh = height(root->right, maxi);
+    maxi = max(maxi, lh + rh);
+    return 1 + max(lh, rh);
+}
+int diameterOfBinaryTree(TreeNode *root)
+{
+    int maxi = INT_MIN;
+    height(root, maxi);
+    return maxi;
+}
+
+// Binary tree maximum path sum (You have to find the sum of nodes which is creating a path and has a maximum value)
+// In this question we have checked the lh and rh with 0 because we know that adding any negative value to any value will decrease the value only so it is better not to add that value to the final result;
+int solve(TreeNode *root, int &maxi)
+{
+    if (root == NULL)
+        return 0;
+    int lh = max(0, solve(root->left, maxi));
+    int rh = max(0, solve(root->right, maxi));
+    maxi = max(maxi, root->val + lh + rh);
+    return root->val + max(lh, rh);
+}
+int maxPathSum(TreeNode *root)
+{
+    int maxi = INT_MIN;
+    solve(root, maxi);
+    return maxi;
+}
+
+// Same Tree(Check if two given trees are same or not)
+bool issame(TreeNode *p, TreeNode *q)
+{
+    if (p == NULL || q == NULL)
+        return (p == q);
+    return (p->val == q->val) && issame(p->left, q->left) && issame(p->right, q->right);
+}
+
+// Zig-Zag tree traversal
+vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+{
+    vector<vector<int>> ans;
+    if (root == NULL)
+        return ans;
+    queue<TreeNode *> q;
+    q.push(root);
+    bool flag = true;
+    while (!q.empty())
+    {
+        int size = q.size();
+        vector<int> store(size);
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode *ele = q.front();
+            q.pop();
+            int index = flag ? i : size - 1 - i;
+            store[index] = ele->val;
+            if (ele->left != NULL)
+            {
+                q.push(ele->left);
+            }
+            if (ele->right != NULL)
+            {
+                q.push(ele->right);
+            }
+        }
+        flag = !flag;
+        ans.push_back(store);
+    }
+    return ans;
+}
+
+// Boundry level traversal of any tree(you have to traverse through the boundry only(left->leaf->right))
+// This code is for the movement in anti-clock wise direction(You can do this in clock wise manner also)
+bool isleaf(Node *root)
+{
+    if (root != NULL && root->left == NULL && root->right == NULL)
+        return true;
+    return false;
+}
+void left(Node *root, vector<int> &ans)
+{
+    Node *cur = root->left;
+    while (cur)
+    {
+        if (!isleaf(cur))
+            ans.push_back(cur->data);
+        if (cur->left != NULL)
+            cur = cur->left;
+        else
+            cur = cur->right;
+    }
+}
+void leaf_node(Node *root, vector<int> &ans)
+{
+    if (root == NULL)
+        return;
+    if (isleaf(root))
+        ans.push_back(root->data);
+    leaf_node(root->left, ans);
+    leaf_node(root->right, ans);
+}
+void right(Node *root, vector<int> &ans)
+{
+    Node *cur = root->right;
+    stack<int> st;
+    while (cur)
+    {
+        if (!isleaf(cur))
+            st.push(cur->data);
+        if (cur->right != NULL)
+            cur = cur->right;
+        else
+            cur = cur->left;
+    }
+    while (!st.empty())
+    {
+        ans.push_back(st.top());
+        st.pop();
+    }
+}
+vector<int> boundary(Node *root)
+{
+    // Your code here
+    vector<int> ans;
+    if (!isleaf(root))
+        ans.push_back(root->data);
+    left(root, ans);
+    leaf_node(root, ans);
+    right(root, ans);
+    return ans;
+}
